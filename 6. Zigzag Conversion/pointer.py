8ab6ee0f0b797c777096cb499eb4672dc4c172a6
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-10-17 23:14:12 +0800

    Create pointer.py

---
+++ 6. Zigzag Conversion/pointer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows ==1:
+            return s
+        strings = ['' for _ in range(numRows)]
+        row , step = 0,1
+        for char in s:
+            strings[row]+=char
+            if row == numRows-1:
+                step = -1
+            elif row == 0:
+                step = 1
+            row +=step
+        return ''.join(strings) 
