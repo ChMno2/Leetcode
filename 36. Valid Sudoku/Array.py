e536183216d64e699874cd31f70f4b13b2b321cc
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-10-17 23:53:10 +0800

    Update Array.py

--- 36. Valid Sudoku/Array.py
+++ 36. Valid Sudoku/Array.py
@@ -1,17 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         def unique(nums):
-            nums = list(filter(lambda x: x!='.',nums))
+            nums = list(filter(lambda x : x != '.',nums))
             return len(set(nums)) == len(nums)
         SW = 9 #sudoku width
         GW = 3 #grid width
         for i in range(SW):
             if not unique(board[i]): #橫的
                 return False
-            if not unique([k[i] for k in board]):
+            if not unique([k[i] for k in board]): #直的
                 return False
-        for i in range(0,SW,GW):
+        for i in range(0,SW,GW): #九宮格
             for j in range(0,SW,GW):
-                if not unique([board[i+m][j+k] for m in range(GW) for k in range(GW)]):
+                if not unique([board[i+m][j+k]for m in range(GW) for k in range(GW)]):
                     return False
         return True
